02c50e63259dd99cab69bef1d5090d39191d8fad
André Vitor Beraldo <beraldo_95@hotmail.com>
2018-11-23 19:08:37 -0200

    Adição arquivo JS + atualização CSS

---
+++ public/js/custom.js
@@ -0,0 +1,12 @@
+
+/* ==== Loader ==== */
+
+$(document).ready(function(){
+  setTimeout(function(){
+    $("body, html").removeClass("no-scroll");
+    $(".spinner").fadeOut();
+    $(".spinner-ctn").fadeOut(600);
+  },700);
+
+});
+
